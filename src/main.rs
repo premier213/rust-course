edcd904ec797cbdf3d6b3f931e31be7701b88d8f
premier213 <premierp1213@gmail.com>
2022-10-19 12:17:03 +0330

    start

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::io;
+
+#[allow(dead_code)]
+fn user_input() {
+    //! user input
+    let mut input = String::new();
+
+    println!("{}", input);
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            println!("you said {}", input);
+        }
+        Err(e) => {
+            println!("somthing {}", e);
+        }
+    };
+}
+
+fn main() {
+    println!("hello")
+}
